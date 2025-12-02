9e84bbd69ff5c0929c185f0c75a55237c25d216a
happyzjp <happyzjp@gmail.com>
2025-12-02 11:27:07 +0800

    Update tasks.py

--- kokoro/task_center/api/tasks.py
+++ kokoro/task_center/api/tasks.py
@@ -13,7 +13,7 @@ from kokoro.task_center.shared import miner_cache
 
 router = APIRouter()
 logger = setup_logger(__name__)
-logger.info("init log test restart python")
+logger.info("init log test restart python 2")
 
 @router.post("/publish", response_model=TaskResponse)
 async def publish_task(
